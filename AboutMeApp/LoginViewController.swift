eb5f58862c4350caf93feeb674fd681c1cea5dde
Maxim Makarov <makarovmaxqa@gmail.com>
2023-11-28 13:17:34 +0100

    Create logic for remind user name and  password

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -16,6 +16,7 @@ final class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        logInButton.layer.cornerRadius = 10
     }
     
     @IBAction func logInButtonTapped() {
@@ -23,13 +24,18 @@ final class LoginViewController: UIViewController {
     
     
     @IBAction func remindUserNameButtonTapped(_ sender: UIButton) {
+        showAlert(withTitle: "Oops!", andMessage: "Your name is 1 😉")
     }
     
     @IBAction func remindPasswordButtonTapped(_ sender: UIButton) {
+        showAlert(withTitle: "Oops!", andMessage: "Your password is 1 😉")
     }
     
-    
-
-
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
