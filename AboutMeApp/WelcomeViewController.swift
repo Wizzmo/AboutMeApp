037500f4f96f4e62b80e3ea7573560824da80f9a
Maxim Makarov <makarovmaxqa@gmail.com>
2023-11-28 14:02:57 +0100

    Add WelcomeViewController

---
+++ AboutMeApp/WelcomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WelcomeViewController.swift
+//  AboutMeApp
+//
+//  Created by Maxim Makarov on 28.11.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
