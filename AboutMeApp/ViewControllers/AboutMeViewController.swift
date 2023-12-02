20893b326b3190c4e33a265c155504abd78b2de5
Maxim Makarov <makarovmaxqa@gmail.com>
2023-12-02 18:23:47 +0100

    Add View Controllers files

---
+++ AboutMeApp/ViewControllers/AboutMeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AboutMeViewController.swift
+//  AboutMeApp
+//
+//  Created by Maxim Makarov on 02.12.2023.
+//
+
+import UIKit
+
+final class AboutMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ AboutMeApp/ViewControllers/BioViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BioViewController.swift
+//  AboutMeApp
+//
+//  Created by Maxim Makarov on 02.12.2023.
+//
+
+import UIKit
+
+final class BioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
